08691cfb703d2ae1563ecf59b5840b8e110f7426
Liubov <drogaltseva92@gmail.com>
2023-12-18 14:47:24 +0100

    task 1(create card of user)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+
+import React from 'react';
+import user from './user.json';
+import css from '../components/Profile.module.css';
+//console.log(css)
+
+export const Profile = () => {
+  return (<div className={css.profile}>
+    <div className= {css.descr}>
+      <img
+        src={user.avatar}
+        alt="User avatar"
+        className= {css.avatar}
+      />
+      <p className={css.userName}>{user.username}</p>
+      <p className={css.tag}>@{user.tag}</p>
+      <p className={css.location}>{user.location}</p>
     </div>
+
+    <ul className={css.stats}>
+      <li>
+        <span className={css.label}>Followers</span>
+        <span className={css.quantity}>{user.stats.followers}</span>
+      </li>
+      <li>
+        <span className={css.label}>Views</span>
+        <span className={css.quantity}>{user.stats.views}</span>
+      </li>
+      <li>
+        <span className={css.label}>Likes</span>
+        <span className={css.quantity}>{user.stats.likes}</span>
+      </li>
+    </ul>
+  </div>
   );
 };
+
