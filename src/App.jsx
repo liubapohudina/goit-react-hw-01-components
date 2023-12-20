43f2943e04c4964a0c464441ad6e9be49134b2e5
Liubov <drogaltseva92@gmail.com>
2023-12-20 16:54:04 +0100

    check props for statistics

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Profile from './components/Profile/Profile';
 import Statistics from './components/Statistics/Statistics';
+// import FriendList from './components/FriendList/FriendList';
 import '../src/index';
 import user from '../src/data/user';
 import data from '../src/data/data';
+// import friends from '../src/data/friends';
 
 function App() {
   return (
@@ -13,7 +15,8 @@ function App() {
        location={user.location}
        avatar={user.avatar}
        stats={user.stats}/>
-      <Statistics title="Upload stats" stats={data}/>
+      <Statistics title="Upload stats" stats={data} />
+      {/* <FriendList friends={friends} />; */}
     </div>
   );
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,4 +1,6 @@
 import css from '../Statistics/Statistics.module.css';
+import PropTypes from "prop-types";
+
 
 const Statistics = ({ title, stats }) => {
 const randomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16);
@@ -10,7 +12,7 @@ const randomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16
       className="item"
     >
       <span className="label">{stat.label}</span>
-      <span className={css.percentage}>{stat.percentage}</span>
+      <span className={css.percentage}>{stat.percentage}%</span>
     </li>
   ));
 
@@ -20,6 +22,17 @@ const randomColor = () => '#' + Math.floor(Math.random() * 16777215).toString(16
       <ul className={css.statList}>{element}</ul>
     </section>
     );
+};
+
+Statistics.propTypes = {
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ).isRequired,
 };
 
 export default Statistics;
